f8cef17f6ba05dc50cb9dd0c7352a2c0db2be67c
Lionel Mason <capitalcookhouse@gmail.com>
2024-12-06 13:02:16 -0500

    completes attributes-and-properties lab

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,36 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+
+
+    def __init__(self, name="Uknwon", breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+
+    # Getter for name
+    def get_name(self):
+        return self._name
+
+    # Setter for name
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # Getter for breed
+    def get_breed(self):
+        return self._breed
+
+    # Setter for breed
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+ 
